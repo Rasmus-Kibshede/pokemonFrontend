300210860c8601c4379c61a5724746865f304529
Rasmus Kibshede <2740ralle@gmail.com>
2022-04-14 23:18:00 +0200

    pokedex v2

--- js/display/pokedexDisplayPokemon.js
+++ js/display/pokedexDisplayPokemon.js
@@ -8,9 +8,16 @@ function displayPokemonInfo(pokeDex) {
   getElement("#pokedexImg").src = pokeDex.sprites.other.dream_world.front_default;
 
   for (let i = 0; i < pokeDex.stats.length; i++) {
-    let tag = document.createElement("span");
-    tag.textContent = pokeDex.stats[i].stat.name + " " + pokeDex.stats[i].base_stat;
-    statsDiv.appendChild(tag);
+
+    let div = document.createElement("div");
+
+    let key = document.createElement("span");
+    key.textContent = pokeDex.stats[i].stat.name.toUpperCase();
+    div.appendChild(key);
+
+    let value = document.createElement("span");
+    value.textContent = pokeDex.stats[i].base_stat;
+    div.appendChild(value);
+    statsDiv.appendChild(div);
   }
 }
-
